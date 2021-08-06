aa00cb4063df8cd3dba97ab0c32b522c6e60e238
Alone Silva <alone.pereira@hotmail.com>
2021-08-06 06:49:58 -0300

    Criando estrutura do projeto

---
+++ src/main/java/br/gov/sp/feiras/FeirasSpApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.feiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeirasSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeirasSpApplication.class, args);
+	}
+
+}
