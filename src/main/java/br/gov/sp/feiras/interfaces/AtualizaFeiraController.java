ae4c044ae28e6dd30ade29751bb8171f6206f3b0
Alone Silva <alone.pereira@hotmail.com>
2021-08-16 14:05:03 -0300

    Chamar AtualizaService pelo Controller AtualizaController. Closes #21

--- src/main/java/br/gov/sp/feiras/interfaces/AtualizaFeiraController.java
+++ src/main/java/br/gov/sp/feiras/interfaces/AtualizaFeiraController.java
@@ -1,6 +1,10 @@
 package br.gov.sp.feiras.interfaces;
 
+import br.gov.sp.feiras.application.atualiza.AtualizaFeiraService;
 import br.gov.sp.feiras.interfaces.dto.FeiraDTO;
+import br.gov.sp.feiras.interfaces.mapper.FeiraDTOMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,10 +17,25 @@ import javax.validation.Valid;
 @RestController
 public class AtualizaFeiraController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AtualizaFeiraController.class);
+
+    private final AtualizaFeiraService service;
+    private final FeiraDTOMapper mapper;
+
+    public AtualizaFeiraController(AtualizaFeiraService service, FeiraDTOMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+
     @PutMapping("/feiras/{id}")
     public ResponseEntity<Object> atualizarFeira(@PathVariable Long id, @Valid @RequestBody FeiraDTO feira) {
+        LOG.info("Inicio do processo de atualização da Feira com ID {}", id);
 
-        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).build();
+        var feiraVO = service.atualizarFeira(mapper.toFeiraVO(id, feira));
+
+        LOG.info("Fim do processo de atualização da Feira com ID {}", id);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(mapper.toFeiraDTORS(feiraVO));
     }
 
 }
--- src/test/java/br/gov/sp/feiras/interfaces/AtualizaFeiraControllerTest.java
+++ src/test/java/br/gov/sp/feiras/interfaces/AtualizaFeiraControllerTest.java
@@ -1,30 +1,52 @@
 package br.gov.sp.feiras.interfaces;
 
+import br.gov.sp.feiras.application.atualiza.AtualizaFeiraService;
+import br.gov.sp.feiras.application.vo.FeiraVO;
 import br.gov.sp.feiras.interfaces.dto.FeiraDTO;
+import br.gov.sp.feiras.interfaces.dto.FeiraDTORS;
+import br.gov.sp.feiras.interfaces.mapper.FeiraDTOMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@ExtendWith(MockitoExtension.class)
 class AtualizaFeiraControllerTest {
 
     private MockMvc mockMvc;
 
+    @Mock
+    private AtualizaFeiraService service;
+
+    @Mock
+    private FeiraDTOMapper mapper;
+
     @BeforeEach
     void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new AtualizaFeiraController()).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(new AtualizaFeiraController(service, mapper)).build();
     }
 
     @Test
     void testAtualizarFeiraComSucesso() throws Exception {
+        Mockito.when(service.atualizarFeira(ArgumentMatchers.any(FeiraVO.class))).thenReturn(montarFeiraVO());
+        Mockito.when(mapper.toFeiraVO(ArgumentMatchers.anyLong(), ArgumentMatchers.any(FeiraDTO.class)))
+                .thenReturn(montarFeiraVO());
+        Mockito.when(mapper.toFeiraDTORS(ArgumentMatchers.any(FeiraVO.class))).thenReturn(montarFeiraDTORS());
+
         mockMvc.perform(MockMvcRequestBuilders.put("/feiras/1")
                 .content(requestFeira())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(responseJson()))
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
     }
 
@@ -57,6 +79,56 @@ class AtualizaFeiraControllerTest {
         feira.setReferencia("PROXIMO DISTRITO POLICIAL");
 
         return new ObjectMapper().writeValueAsString(feira);
+    }
+
+    private FeiraVO montarFeiraVO() {
+        FeiraVO feira = new FeiraVO();
+
+        feira.setId(1L);
+        feira.setLat(-46731043D);
+        feira.setLon(-23592640D);
+        feira.setSetcens("355030894000165");
+        feira.setAreaP("3550308005179");
+        feira.setCodDist(96);
+        feira.setDistrito("VILA SONIA");
+        feira.setCodigoSubPrefeitura(10);
+        feira.setSubPrefeitura("BUTANTA");
+        feira.setRegiao5("Oeste");
+        feira.setRegiao8("Oeste");
+        feira.setNomeFeira("VILA SONIA");
+        feira.setRegistro("6034-8");
+        feira.setLogradouro("AV GAL FRANCISCO MORAZAN");
+        feira.setBairro("VL SONIA");
+        feira.setReferencia("PROXIMO DISTRITO POLICIAL");
+
+        return feira;
+    }
+
+    private FeiraDTORS montarFeiraDTORS() {
+        FeiraDTORS feira = new FeiraDTORS();
+
+        feira.setId(1L);
+        feira.setLat(-46731043D);
+        feira.setLon(-23592640D);
+        feira.setSetcens("355030894000165");
+        feira.setAreaP("3550308005179");
+        feira.setCodDist(96);
+        feira.setDistrito("VILA SONIA");
+        feira.setCodigoSubPrefeitura(10);
+        feira.setSubPrefeitura("BUTANTA");
+        feira.setRegiao5("Oeste");
+        feira.setRegiao8("Oeste");
+        feira.setNomeFeira("VILA SONIA");
+        feira.setRegistro("6034-8");
+        feira.setLogradouro("AV GAL FRANCISCO MORAZAN");
+        feira.setBairro("VL SONIA");
+        feira.setReferencia("PROXIMO DISTRITO POLICIAL");
+
+        return feira;
+    }
+
+    private String responseJson() throws Exception {
+        return new ObjectMapper().writeValueAsString(montarFeiraDTORS());
     }
 
 }
